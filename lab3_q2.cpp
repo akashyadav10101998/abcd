d3746d1dbd27b87f894fbcfed35736ef09f25747
akashyadav10101998 <42208424+akashyadav10101998@users.noreply.github.com>
2018-08-17 17:42:44 +0530

    Add files via upload

---
+++ lab3_q2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int a,b,sum,subtract,product,remainder;
+float division;
+;a = 5
+;b = 3
+;sum = a + b;
+;subtract = a - b;
+;product = a * b;
+;division = a / b;
+;remainder = a % b
+;cout << "number are "<< a << "," << b << endl;
+;cout << "results in integer" << endl;
+;cout << "sum is " << sum << endl;
+;cout << "difference is " << subtract << endl;
+;cout << "product is " << product << endl;
+;cout << "quotient is " << division << endl;
+;cout << "remainder is " <<remainder << endl;
+return 0;
+}
